95fef07928951e517e1468007cc5961c5228dfea
Ahmed Raza <0326ahmedaptech@gmail.com>
2023-09-28 00:32:28 -0700

    This is files and folders in daraz folder

---
+++ js/index.js
@@ -0,0 +1,298 @@
+var fs_cards = [
+    {
+        fs_cards_image : 'images/products/product-image-1.jpg',
+        fs_card_title : 'Coller Mic for Youtube',
+        fs_card_price : 'Rs. 200',
+        fs_card_real_price : 'Rs. 400',
+        fs_card_disc : '10%'
+    },
+    {
+        fs_cards_image : 'images/products/product-image-2.jpg',
+        fs_card_title : 'Money Gun',
+        fs_card_price : 'Rs. 500',
+        fs_card_real_price : 'Rs. 700',
+        fs_card_disc : '12%'
+    },
+    {
+        fs_cards_image : 'images/products/product-image-3.jpg',
+        fs_card_title : 'Onion Hair Oil',
+        fs_card_price : 'Rs. 700',
+        fs_card_real_price : 'Rs. 900',
+        fs_card_disc : '8%'
+    },
+    {
+        fs_cards_image : 'images/products/product-image-4.webp',
+        fs_card_title : 'Hand Braslet for Men & Women',
+        fs_card_price : 'Rs. 400',
+        fs_card_real_price : 'Rs. 600',
+        fs_card_disc : '12%'
+    },
+    {
+        fs_cards_image : 'images/products/product-image-5.webp',
+        fs_card_title : 'Black & Golden colored Watch',
+        fs_card_price : 'Rs. 1650',
+        fs_card_real_price : 'Rs. 2220',
+        fs_card_disc : '18%'
+    },
+    {
+        fs_cards_image : 'images/products/product-image-6.webp',
+        fs_card_title : 'Black Colored Wooden Walk watch',
+        fs_card_price : 'Rs. 1500',
+        fs_card_real_price : 'Rs. 2000',
+        fs_card_disc : '25%'
+    },
+]
+let product_cards = [
+    {
+        product_card_image : 'images/products/product-image-1.jpg',
+        product_card_title : 'Coller Mic for Youtube',
+        product_card_price : 'Rs. 200',
+        product_card_real_price : 'Rs. 400',
+        product_card_disc : '10%'
+    },
+    {
+        product_card_image : 'images/products/product-image-2.jpg',
+        product_card_title : 'Money Gun',
+        product_card_price : 'Rs. 500',
+        product_card_real_price : 'Rs. 700',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-3.jpg',
+        product_card_title : 'Onion Hair Oil',
+        product_card_price : 'Rs. 700',
+        product_card_real_price : 'Rs. 900',
+        product_card_disc : '8%'
+    },
+    {
+        product_card_image : 'images/products/product-image-4.webp',
+        product_card_title : 'Hand Braslet for Men & Women',
+        product_card_price : 'Rs. 400',
+        product_card_real_price : 'Rs. 600',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-5.webp',
+        product_card_title : 'Black & Golden colored Watch',
+        product_card_price : 'Rs. 1650',
+        product_card_real_price : 'Rs. 2220',
+        product_card_disc : '18%'
+    },
+    {
+        product_card_image : 'images/products/product-image-6.webp',
+        product_card_title : 'Black Colored Wooden Walk watch',
+        product_card_price : 'Rs. 1500',
+        product_card_real_price : 'Rs. 2000',
+        product_card_disc : '25%'
+    },
+    {
+        product_card_image : 'images/products/product-image-1.jpg',
+        product_card_title : 'Coller Mic for Youtube',
+        product_card_price : 'Rs. 200',
+        product_card_real_price : 'Rs. 400',
+        product_card_disc : '10%'
+    },
+    {
+        product_card_image : 'images/products/product-image-2.jpg',
+        product_card_title : 'Money Gun',
+        product_card_price : 'Rs. 500',
+        product_card_real_price : 'Rs. 700',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-3.jpg',
+        product_card_title : 'Onion Hair Oil',
+        product_card_price : 'Rs. 700',
+        product_card_real_price : 'Rs. 900',
+        product_card_disc : '8%'
+    },
+    {
+        product_card_image : 'images/products/product-image-4.webp',
+        product_card_title : 'Hand Braslet for Men & Women',
+        product_card_price : 'Rs. 400',
+        product_card_real_price : 'Rs. 600',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-5.webp',
+        product_card_title : 'Black & Golden colored Watch',
+        product_card_price : 'Rs. 1650',
+        product_card_real_price : 'Rs. 2220',
+        product_card_disc : '18%'
+    },
+    {
+        product_card_image : 'images/products/product-image-6.webp',
+        product_card_title : 'Black Colored Wooden Walk watch',
+        product_card_price : 'Rs. 1500',
+        product_card_real_price : 'Rs. 2000',
+        product_card_disc : '25%'
+    },
+    {
+        product_card_image : 'images/products/product-image-1.jpg',
+        product_card_title : 'Coller Mic for Youtube',
+        product_card_price : 'Rs. 200',
+        product_card_real_price : 'Rs. 400',
+        product_card_disc : '10%'
+    },
+    {
+        product_card_image : 'images/products/product-image-2.jpg',
+        product_card_title : 'Money Gun',
+        product_card_price : 'Rs. 500',
+        product_card_real_price : 'Rs. 700',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-3.jpg',
+        product_card_title : 'Onion Hair Oil',
+        product_card_price : 'Rs. 700',
+        product_card_real_price : 'Rs. 900',
+        product_card_disc : '8%'
+    },
+    {
+        product_card_image : 'images/products/product-image-4.webp',
+        product_card_title : 'Hand Braslet for Men & Women',
+        product_card_price : 'Rs. 400',
+        product_card_real_price : 'Rs. 600',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-5.webp',
+        product_card_title : 'Black & Golden colored Watch',
+        product_card_price : 'Rs. 1650',
+        product_card_real_price : 'Rs. 2220',
+        product_card_disc : '18%'
+    },
+    {
+        product_card_image : 'images/products/product-image-6.webp',
+        product_card_title : 'Black Colored Wooden Walk watch',
+        product_card_price : 'Rs. 1500',
+        product_card_real_price : 'Rs. 2000',
+        product_card_disc : '25%'
+    },
+    {
+        product_card_image : 'images/products/product-image-1.jpg',
+        product_card_title : 'Coller Mic for Youtube',
+        product_card_price : 'Rs. 200',
+        product_card_real_price : 'Rs. 400',
+        product_card_disc : '10%'
+    },
+    {
+        product_card_image : 'images/products/product-image-2.jpg',
+        product_card_title : 'Money Gun',
+        product_card_price : 'Rs. 500',
+        product_card_real_price : 'Rs. 700',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-3.jpg',
+        product_card_title : 'Onion Hair Oil',
+        product_card_price : 'Rs. 700',
+        product_card_real_price : 'Rs. 900',
+        product_card_disc : '8%'
+    },
+    {
+        product_card_image : 'images/products/product-image-4.webp',
+        product_card_title : 'Hand Braslet for Men & Women',
+        product_card_price : 'Rs. 400',
+        product_card_real_price : 'Rs. 600',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-5.webp',
+        product_card_title : 'Black & Golden colored Watch',
+        product_card_price : 'Rs. 1650',
+        product_card_real_price : 'Rs. 2220',
+        product_card_disc : '18%'
+    },
+    {
+        product_card_image : 'images/products/product-image-6.webp',
+        product_card_title : 'Black Colored Wooden Walk watch',
+        product_card_price : 'Rs. 1500',
+        product_card_real_price : 'Rs. 2000',
+        product_card_disc : '25%'
+    },
+    {
+        product_card_image : 'images/products/product-image-1.jpg',
+        product_card_title : 'Coller Mic for Youtube',
+        product_card_price : 'Rs. 200',
+        product_card_real_price : 'Rs. 400',
+        product_card_disc : '10%'
+    },
+    {
+        product_card_image : 'images/products/product-image-2.jpg',
+        product_card_title : 'Money Gun',
+        product_card_price : 'Rs. 500',
+        product_card_real_price : 'Rs. 700',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-3.jpg',
+        product_card_title : 'Onion Hair Oil',
+        product_card_price : 'Rs. 700',
+        product_card_real_price : 'Rs. 900',
+        product_card_disc : '8%'
+    },
+    {
+        product_card_image : 'images/products/product-image-4.webp',
+        product_card_title : 'Hand Braslet for Men & Women',
+        product_card_price : 'Rs. 400',
+        product_card_real_price : 'Rs. 600',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-5.webp',
+        product_card_title : 'Black & Golden colored Watch',
+        product_card_price : 'Rs. 1650',
+        product_card_real_price : 'Rs. 2220',
+        product_card_disc : '18%'
+    },
+    {
+        product_card_image : 'images/products/product-image-6.webp',
+        product_card_title : 'Black Colored Wooden Walk watch',
+        product_card_price : 'Rs. 1500',
+        product_card_real_price : 'Rs. 2000',
+        product_card_disc : '25%'
+    },
+    {
+        product_card_image : 'images/products/product-image-1.jpg',
+        product_card_title : 'Coller Mic for Youtube',
+        product_card_price : 'Rs. 200',
+        product_card_real_price : 'Rs. 400',
+        product_card_disc : '10%'
+    },
+    {
+        product_card_image : 'images/products/product-image-2.jpg',
+        product_card_title : 'Money Gun',
+        product_card_price : 'Rs. 500',
+        product_card_real_price : 'Rs. 700',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-3.jpg',
+        product_card_title : 'Onion Hair Oil',
+        product_card_price : 'Rs. 700',
+        product_card_real_price : 'Rs. 900',
+        product_card_disc : '8%'
+    },
+    {
+        product_card_image : 'images/products/product-image-4.webp',
+        product_card_title : 'Hand Braslet for Men & Women',
+        product_card_price : 'Rs. 400',
+        product_card_real_price : 'Rs. 600',
+        product_card_disc : '12%'
+    },
+    {
+        product_card_image : 'images/products/product-image-5.webp',
+        product_card_title : 'Black & Golden colored Watch',
+        product_card_price : 'Rs. 1650',
+        product_card_real_price : 'Rs. 2220',
+        product_card_disc : '18%'
+    },
+    {
+        product_card_image : 'images/products/product-image-6.webp',
+        product_card_title : 'Black Colored Wooden Walk watch',
+        product_card_price : 'Rs. 1500',
+        product_card_real_price : 'Rs. 2000',
+        product_card_disc : '25%'
+    },
+]
